93ea1410ebd40ee8aeab6b6bec7e394261dc5475
Nikodimos Bacos <nikodimosbacos@gmail.com>
2025-04-05 00:24:02 +0300

    string

--- strings.py
+++ strings.py
@@ -1,2 +1,10 @@
-s = 'niko'
-print(s.upper())
+# Examples for single, double and Triple quotes
+#Single quote
+string = 'hello world'
+print(string)
+#Double quote 
+stringg = "He's a doctor"
+print(stringg)
+#Triple quote
+stringgg = """ My teacher once said to me "The only permanent thing is change So, learn to adapt."and it stuck in my mind"""
+print(stringgg)
