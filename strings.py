c0bf031247d691af7c3c1e33dd1926be083b5488
Nikodimos Bacos <nikodimosbacos@gmail.com>
2025-04-05 02:21:03 +0300

    string

--- strings.py
+++ strings.py
@@ -8,3 +8,28 @@ print(stringg)
 #Triple quote
 stringgg = """ My teacher once said to me "The only permanent thing is change So, learn to adapt."and it stuck in my mind"""
 print(stringgg)
+
+# Example for string.upper()
+name = "Nikodimos Bacos"
+print (name.upper())
+
+# Example for string.lower()
+user = "HELLO"
+print (user.lower())
+
+# Example for string.title()
+Psalm1 = "I have been young, and now am old; yet have I not seen the righteous forsaken, nor his seed begging bread."
+print (Psalm1.title())
+
+# Example for str.capitalize()
+God = "jesus CHRIST"
+print(God.capitalize())
+
+# Example for str.swapcase()
+Psalm2 = "'tHE lORD IS MY SHEPHERD; i LACK NOTHING.'"
+print(Psalm2.swapcase())
+
+#Example for str.find()
+Psalm3 = "For whosoever hath, to him shall be given, and he shall have more abundance: but whosoever hath not, from him shall be taken away even that he hath."
+print(Psalm3.find('abundance'))
+print(Psalm3.find('nikodimos'))
